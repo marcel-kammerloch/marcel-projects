a9d66dda3d98d091f373e5eab8c5ebca6c08c886
Marcel K <developer.marcel1@gmail.com>
2025-12-29 21:43:03 +0100

    updated csp for music-player

--- apps/music-player/next.config.ts
+++ apps/music-player/next.config.ts
@@ -7,6 +7,11 @@ const nextConfig: NextConfig = {
         source: "/(.*)",
         headers: [
           {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com; media-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com;",
+          },
+          {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
           },
