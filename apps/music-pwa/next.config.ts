8af6d14ee4d44e6e43b57ce1059a7de26176725c
Marcel K <developer.marcel1@gmail.com>
2026-04-03 19:05:34 +0200

    fixed audio preview not working

--- apps/music-pwa/next.config.ts
+++ apps/music-pwa/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com; media-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com; media-src 'self' blob: https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com;",
           },
           {
             key: "Referrer-Policy",
