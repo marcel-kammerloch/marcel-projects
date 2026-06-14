edc3857fd75d6e8fac869e6ffdfd992faa4d50c6
Marcel K <developer.marcel1@gmail.com>
2026-06-14 11:03:13 +0200

    updated csp

--- apps/music-pwa/next.config.ts
+++ apps/music-pwa/next.config.ts
@@ -10,11 +10,11 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value:
               `default-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com;
-              script-src 'self' 'unsafe-inline' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : "'wasm-unsafe-eval'"} https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js;
               style-src 'self' 'unsafe-inline';
               img-src 'self';
-              connect-src 'self' https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm https://vercel.com/api/blob;
-              media-src 'self' blob:;`
+              connect-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm https://vercel.com/api/blob;
+              media-src 'self' blob:; https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com`
                 .replace(/\s{2,}/g, " ")
                 .trim(),
           },
