d15183b09c9596864eeb94753422c92dda90d9bf
Marcel K <developer.marcel1@gmail.com>
2026-06-14 10:57:00 +0200

    fixed csp error

--- apps/music-pwa/next.config.ts
+++ apps/music-pwa/next.config.ts
@@ -8,7 +8,15 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: `default-src 'self'; script-src 'self' 'unsafe-inline' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""} https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js; style-src 'self' 'unsafe-inline'; img-src 'self'; connect-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm https://vercel.com/api/blob; media-src 'self' blob: https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com;`,
+            value:
+              `default-src 'self' https://ad0nzrqxbs7k6ri0.public.blob.vercel-storage.com;
+              script-src 'self' 'unsafe-inline' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : "'wasm-unsafe-eval'"} https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.js;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self';
+              connect-src 'self' https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd/ffmpeg-core.wasm https://vercel.com/api/blob;
+              media-src 'self' blob:;`
+                .replace(/\s{2,}/g, " ")
+                .trim(),
           },
           {
             key: "Referrer-Policy",
