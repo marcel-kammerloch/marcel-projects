131b3c589d84a45927bd083f728ec3ec48c2f492
Marcel K <developer.marcel1@gmail.com>
2026-07-02 21:51:12 +0200

    removed neon client

--- packages/auth/prisma.ts
+++ packages/auth/prisma.ts
@@ -1,16 +1,12 @@
-import { Client } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "./generated/client";
 
-const connectionString = process.env.MARCEL_PROJECTS_DATABASE_URL;
+const connectionString = process.env.MARCEL_PROJECTS_DATABASE_URL!;
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-const client = new Client({ connectionString });
-const adapter = new PrismaNeon(client);
+const adapter = new PrismaNeon({ connectionString });
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export * from "./generated/client";
