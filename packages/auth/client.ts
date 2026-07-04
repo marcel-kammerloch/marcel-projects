e9d7dc20e2c8c735718b724cf77d122fcf9d8c59
Marcel K <developer.marcel1@gmail.com>
2026-07-04 09:36:20 +0200

    added custom session and inferAdditionalFields plugins

--- packages/auth/client.ts
+++ packages/auth/client.ts
@@ -1,15 +1,21 @@
 import { AUTH_URL } from "@repo/utils";
 import { createAuthClient } from "better-auth/react";
+import {
+  customSessionClient,
+  inferAdditionalFields,
+} from "better-auth/client/plugins";
+import type { auth } from "./server";
 
 export const authClient = createAuthClient({
   baseURL: AUTH_URL,
+  plugins: [
+    customSessionClient<typeof auth>(),
+    inferAdditionalFields<typeof auth>(),
+  ],
 });
 
 export const useAuth = () => {
   const { data } = authClient.useSession();
 
-  const role =
-    data && data.user && "role" in data.user ? data.user.role : "user";
-
-  return { isAdmin: role === "admin" };
+  return { isAdmin: data?.user.role === "admin" };
 };
