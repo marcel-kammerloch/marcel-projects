d2aa2067a51c94f31f0c5a95ef8ff9444fbe4f46
Marcel K <developer.marcel1@gmail.com>
2026-08-17 19:55:26 +0200

    removed unused import

--- apps/music/src/components/upload/AudioWaveform.tsx
+++ apps/music/src/components/upload/AudioWaveform.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState, useMemo } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface AudioWaveformProps {
   file: File | null;
