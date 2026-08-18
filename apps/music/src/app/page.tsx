8c54251e14dc162a6648e3885afe55a3841d39e0
Marcel K <developer.marcel1@gmail.com>
2026-08-18 19:30:56 +0200

    removed suspense because data is cached

--- apps/music/src/app/page.tsx
+++ apps/music/src/app/page.tsx
@@ -1,9 +1,10 @@
 import { getSongs } from "@/actions/song";
 import SongList from "@/components/song/SongList";
 import UploadModalBtn from "@/components/upload/UploadModalBtn";
-import { Suspense } from "react";
 
 export default async function Home() {
+  const { data: songs } = await getSongs();
+
   return (
     <main className="flex-1 w-full max-w-4xl mx-auto px-2 md:px-4 mt-8">
       <div className="flex items-center justify-between mb-8">
@@ -14,18 +15,8 @@ export default async function Home() {
       </div>
 
       <div className="space-y-4">
-        <Suspense fallback={<div>Loading</div>}>
-          <Songs />
-        </Suspense>
+        <SongList initialSongs={songs || []} />;
       </div>
     </main>
   );
-}
-
-async function Songs() {
-    const { data: songs } = await getSongs();
-
-    return (
-      <SongList initialSongs={songs || []} />
-    )
 }
